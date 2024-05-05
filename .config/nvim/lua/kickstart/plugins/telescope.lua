16326655ac198b81b351848f8642b25d2f747d29
jake-edmonstone <jbedmonstone@gmail.com>
2024-05-05 18:25:58 -0400

    made <C-q> open telescope findings in trouble

--- .config/nvim/lua/kickstart/plugins/telescope.lua
+++ .config/nvim/lua/kickstart/plugins/telescope.lua
@@ -31,15 +31,27 @@ return {
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
+      local actions = require("telescope.actions")
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = {
+              -- This uses trouble for the quickfix list instead of the default
+              ["<C-q>"] = function(prompt_bufnr)
+                actions.smart_send_to_qflist(prompt_bufnr)
+                vim.cmd("Trouble quickfix")
+              end,
+            },
+            n = {
+              ["<C-q>"] = function(prompt_bufnr)
+                actions.smart_send_to_qflist(prompt_bufnr)
+                vim.cmd("Trouble quickfix")
+              end,
+            },
+          },
+        },
         pickers = {
           find_files = {
             find_command = {'fd', '--type', 'f', '--hidden', '--strip-cwd-prefix'}
