71e6f081174b37e348724352508362d608296a48
jake-edmonstone <jbedmonstone@gmail.com>
2025-07-23 01:45:12 -0400

    small changes

--- .config/nvim/lua/plugins/base.lua
+++ .config/nvim/lua/plugins/base.lua
@@ -5,29 +5,6 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  {
-    "Mofiqul/dracula.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("dracula").setup({
-        transparent_bg = true,
-      })
-      vim.cmd.colorscheme("dracula")
-      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2E303E" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
-    end,
-  },
-
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        theme = "dracula-nvim",
-      },
-    },
-  },
-
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
